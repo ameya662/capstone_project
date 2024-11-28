5501b9bfd1e19d77c995b9f03e67617ca3768f05
ameya662 <ameyaexcl@gmail.com>
2024-11-28 15:52:18 +0100

    igw rectified

--- igw.tf
+++ igw.tf
@@ -2,6 +2,10 @@ resource "aws_internet_gateway" "lab_igw" {
   vpc_id = aws_vpc.lab_vpc.id
 }
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.lab_vpc.id
+}
+
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
