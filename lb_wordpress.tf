cb75480fef716c5ed5e0380862e64aa4d084e702
ameya662 <ameyaexcl@gmail.com>
2024-12-04 15:05:11 +0100

    added user data directly

--- lb_wordpress.tf
+++ lb_wordpress.tf
@@ -39,7 +39,14 @@ resource "aws_launch_template" "wordpresslt" {
   vpc_security_group_ids = [resource.aws_security_group.wordpress_sg.id]
 
   # Reference the external user data file
-  user_data = file("userdata_wordpress.sh")
+  user_data = <<EOF
+
+  #!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd 
+sudo systemctl start httpd
+sudo systemctl enable httpd
+EOF
 
   tag_specifications {
     resource_type = "instance"
