9c6f67d9c1e593a3f7e0ae3bc378636127b47cbe
ameya662 <ameyaexcl@gmail.com>
2024-12-14 23:38:08 +0100

    changes in secret

--- main.tf
+++ main.tf
@@ -18,45 +18,45 @@ provider "newrelic" {
   region     = "US"             # Valid values: "US" or "EU"
 }
 
-# Declare the variable for the New Relic API Key
-variable "newrelic_api_key" {
-  type        = string
-  sensitive   = true
-  nullable    = true
-}
+# # Declare the variable for the New Relic API Key
+# variable "newrelic_api_key" {
+#   type        = string
+#   sensitive   = true
+#   nullable    = true
+# }
 
-# AWS Provider Configuration
-variable "AWS_ACCESS_KEY_ID" {
-  type        = string
-  sensitive   = true
-  nullable    = true
-}
+# # AWS Provider Configuration
+# variable "AWS_ACCESS_KEY_ID" {
+#   type        = string
+#   sensitive   = true
+#   nullable    = true
+# }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
-  sensitive   = true
-  nullable    = true
-}
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   type        = string
+#   sensitive   = true
+#   nullable    = true
+# }
 
-variable "AWS_SESSION_TOKEN" {
-  type        = string
-  sensitive   = true
-  nullable    = true
-}
+# variable "AWS_SESSION_TOKEN" {
+#   type        = string
+#   sensitive   = true
+#   nullable    = true
+# }
 
-variable "AWS_DEFAULT_REGION" {
-  type        = string
-  default     = "us-west-2"  # Default region, can be customized
-}
+# variable "AWS_DEFAULT_REGION" {
+#   type        = string
+#   default     = "us-west-2"  # Default region, can be customized
+# }
 
-variable "PER_ACCESS_KEY_ID" {
-  type        = string
-  sensitive   = true
-  nullable    = true
-}
+# variable "PER_ACCESS_KEY_ID" {
+#   type        = string
+#   sensitive   = true
+#   nullable    = true
+# }
 
-variable "PER_SECRET_ACCESS_KEY" {
-  type        = string
-  sensitive   = true
-  nullable    = true
-}
+# variable "PER_SECRET_ACCESS_KEY" {
+#   type        = string
+#   sensitive   = true
+#   nullable    = true
+# }
