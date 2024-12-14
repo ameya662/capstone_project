c448af833c5da2b6fed0db73754a10e5291c8cd3
ameya662 <ameyaexcl@gmail.com>
2024-12-14 21:03:24 +0100

    new

--- main.tf
+++ main.tf
@@ -6,3 +6,20 @@ terraform {
     }
   }
 }
+
+# Configure terraform
+terraform {
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 2.44"
+    }
+  }
+}
+
+# Configure the New Relic provider
+provider "newrelic" {
+  account_id = 6264788
+  api_key = NRAK-LWRZZ1TUQLMPF4NQIYEGJR0EN8M    # usually prefixed with 'NRAK'
+  region = "US"                    # Valid regions are US and EU
+}
