696028a05267e176c57f7edead4e7271d423766d
ameya662 <ameyaexcl@gmail.com>
2024-12-14 23:04:04 +0100

    changes in secret

--- new_relic.tf
+++ new_relic.tf
@@ -1,5 +1,5 @@
 module "newrelic-aws-cloud-integrations" {
-  source = "newrelic/cloud-integrations/newrelic/aws"
+  source = "source = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws"
 
   newrelic_account_id     = 6264788
   newrelic_account_region = "US"
