22c75382e2085466a9d9bd3b8da8c85ed3d3cccd
ameya662 <ameyaexcl@gmail.com>
2024-12-15 22:33:02 +0100

    variable settings

--- main.tf
+++ main.tf
@@ -19,33 +19,32 @@ terraform {
 # }
 
 # AWS Provider Configuration
-# variable "AWS_ACCESS_KEY_ID" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "AWS_SESSION_TOKEN" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "AWS_SESSION_TOKEN" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "AWS_DEFAULT_REGION" {
-#   type        = string
-#   default     = "us-west-2"  # Default region, can be customized
-#   sensitive   = true
-# }
+variable "AWS_DEFAULT_REGION" {
+  type        = string
+  default     = "us-west-2"  # Default region, can be customized
+}
 
-# variable "PER_ACCESS_KEY_ID" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "PER_ACCESS_KEY_ID" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "PER_SECRET_ACCESS_KEY" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "PER_SECRET_ACCESS_KEY" {
+  type        = string
+  sensitive   = true
+}
