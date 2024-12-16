489ff4ede66767bdf6e5b2b7be3f45af4ee5334d
ameya662 <ameyaexcl@gmail.com>
2024-12-16 20:27:51 +0100

    variable set

---
+++ variables.tf
@@ -0,0 +1,37 @@
+# Declare the variable for the New Relic API Key
+# variable "newrelic_api_key" {
+#   type        = string
+#   description = "New Relic API Key"
+#   sensitive = true
+# }
+
+# AWS Provider Configuration
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SESSION_TOKEN" {
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_DEFAULT_REGION" {
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "PER_ACCESS_KEY_ID" {
+  type        = string
+  sensitive   = true
+}
+
+variable "PER_SECRET_ACCESS_KEY" {
+  type        = string
+  sensitive   = true
+}
