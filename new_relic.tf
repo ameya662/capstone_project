a4917af1e50c2e1814cd542fd1d62dbd89eb2d46
ameya662 <ameyaexcl@gmail.com>
2024-12-18 21:00:20 +0100

    newrelic int

--- new_relic.tf
+++ new_relic.tf
@@ -1,12 +1,12 @@
-module "newrelic-aws-cloud-integrations" {
-  source = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws"
+# module "newrelic-aws-cloud-integrations" {
+#   source = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws"
 
-  newrelic_account_id     = 6264788
-  newrelic_account_region = "US"
-  name                    = "production"
+#   newrelic_account_id     = 6264788
+#   newrelic_account_region = "US"
+#   name                    = "production"
 
-  include_metric_filters = {
-    "AWS/EC2" = [],
-    "AWS/RDS" = [],
-  }
-}
+#   include_metric_filters = {
+#     "AWS/EC2" = [],
+#     "AWS/RDS" = [],
+#   }
+# }
